f6d30519669867b29b96caaaad070c075ccf03b7
Isidro-Molina <isidromolina260@gmail.com>
2023-09-07 11:08:38 -0300

    vista producto individual

--- src/commons/Card.js
+++ src/commons/Card.js
@@ -1,27 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const Card = ({product}) => {
-  return (
-    <>
-      <div className="card">
-        <div className="card-image">
-          <figure className="image">
-            <img
-              src={product.img}
-              alt="product img"
-            />
-          </figure>
-        </div>
-        <div className="card-content">
-          <div className="media">
-            <div className="media-content">
-              <p className="title is-6">{product.name}</p>
+const Card = ({ product, onShowMore }) => {
+    return (
+        <>
+            <div className="card">
+                <div className="card-image">
+                    <figure className="image">
+                        <img src={product.img} alt="product img" />
+                    </figure>
+                </div>
+                <div className="card-content">
+                    <div className="media">
+                        <div className="media-content">
+                            <p className="title is-6">{product.name}</p>
+                            <button onClick={onShowMore}>ver mas</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </>
-  )
-}
+        </>
+    );
+};
 
-export default Card
+export default Card;
