a92604ec533ab60242cb2a710a2f9e9cf436dc44
Agustin Torroja <atorroja18@gmail.com>
2023-09-07 10:28:44 -0300

    Config store y conectar Redux con React

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import "bulma/css/bulma.min.css";
 
-import App from './App';
+import { Provider } from "react-redux";
+import store from "./state/store";
 
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-
