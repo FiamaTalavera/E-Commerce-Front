79426634cc6d71aaa70626521ade8f61ab1b5dc0
Isidro-Molina <isidromolina260@gmail.com>
2023-09-08 16:29:22 -0300

    boton

--- src/commons/CardDetails.js
+++ src/commons/CardDetails.js
@@ -1,37 +1,33 @@
 import React from "react";
 
-export const CardDetails = ({ product, onClose }) => {
+export const CardDetails = ({ product, onClose, addToCart }) => {
   return (
-    <div className="modal is-active ">
-      <div className="modal-background">
-        <div className="modal-content custom-modal-content">
-          <div className="column is-vcentered">
-            <header className="modal-card-head">
-              <h2 className="modal-card-title has-text-centered">{product.name}</h2>
-              <button className="delete" onClick={onClose} />
-            </header>
-            <section className="modal-card-body">
-              <div className="columns">
-                <div className="column is-half is-flex is-justify-content-center">
-                  <img 
-                  src={product.imageURL}
-                  alt="no hay fotito"
-                  style={{ width: '320px', height: '320px' }}
-                  />
-                </div>
-                <div className="column is-half">
-                  <h5 className="title is-5">{product.description}</h5>
-                  <p>STOCK: {product.stock}</p>
-                  <h4 className="subtitle is-4">PRECIO: {product.price}</h4>
-                  <br/>
-                  <p>PUNTUACION: </p>
-                  <p>BOTON AGREGAR AL CARRITO</p>
-                </div>
+      <div className="modal is-active ">
+          <div className="modal-background">
+              <div className="modal-content custom-modal-content">
+                  <div className="column is-vcentered">
+                      <header className="modal-card-head">
+                          <h2 className="modal-card-title has-text-centered">{product.name}</h2>
+                          <button className="delete" onClick={onClose} />
+                      </header>
+                      <section className="modal-card-body">
+                          <div className="columns">
+                              <div className="column is-half is-flex is-justify-content-center">
+                                  <img src={product.imageURL} alt="no hay fotito" style={{ width: '320px', height: '320px' }} />
+                              </div>
+                              <div className="column is-half">
+                                  <h5 className="title is-5">{product.description}</h5>
+                                  <p>STOCK: {product.stock}</p>
+                                  <h4 className="subtitle is-4">PRECIO: {product.price}</h4>
+                                  <br />
+                                  <p>PUNTUACION: </p>
+                                  <button onClick={() => addToCart(product)}>Agregar al chango</button>
+                              </div>
+                          </div>
+                      </section>
+                  </div>
               </div>
-            </section>
           </div>
-        </div>
       </div>
-    </div>
   );
 };
