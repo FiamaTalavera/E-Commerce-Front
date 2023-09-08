3510cbecc8bc1f5dc5e9325d71763f8fd215e6d3
Agustin Torroja <atorroja18@gmail.com>
2023-09-08 15:59:48 -0300

    modificacion  de estilo sobre CardDetails y Grid menu

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -32,7 +32,7 @@ const Grid = () => {
     return (
         <>
             <div className="container text-center">
-                <h1 className="title is-3 has-text-centered">Menu</h1>
+                <h1 className="title is-3 has-text-centered p-6">Menu</h1>
                 <div className="columns is-multiline">
                     {products.map((product, i) => (
                         <div className="column is-4" key={i}>
