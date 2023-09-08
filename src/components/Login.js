2044194935ad7a3f50e5c57c0567596281885b12
FiamaTalavera <fiama.viccini@gmail.com>
2023-09-07 22:50:22 -0300

    Se crea el componente login, falta validaciones y prueba con back

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios'; 
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    axios
+      .post('/login', { email, password })
+      .then((response) => {
+        if (response.status === 200) {
+          navigate('/');
+        } else {
+          console.error('Error de inicio de sesión');
+        }
+      })
+      .catch((error) => {
+        console.error('Error de red o del servidor:', error);
+      });
+  };
+
   return (
     <section className="section">
       <div className="container">
@@ -11,18 +32,38 @@ const Login = () => {
               <div className="field">
                 <label className="label">Correo electrónico</label>
                 <div className="control">
-                  <input className="input" type="email" placeholder="correo@example.com" />
+                  <input
+                    className="input"
+                    type="email"
+                    placeholder="correo@ejemplo.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </div>
               </div>
               <div className="field">
                 <label className="label">Contraseña</label>
                 <div className="control">
-                  <input className="input" type="password" placeholder="Contraseña" />
+                  <input
+                    className="input"
+                    type="password"
+                    placeholder="Contraseña"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                 </div>
               </div>
               <div className="field">
                 <div className="control">
-                  <button className="button is-primary">Iniciar sesión</button>
+                  <button
+                    className="button is-primary"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleLogin();
+                    }}
+                  >
+                    Iniciar sesión
+                  </button>
                 </div>
               </div>
             </form>
@@ -31,6 +72,6 @@ const Login = () => {
       </div>
     </section>
   );
-}
+};
 
 export default Login;
