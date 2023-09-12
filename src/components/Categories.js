b31d15e951f57385cd0d6aae9c57f84a19088eac
FiamaTalavera <fiama.viccini@gmail.com>
2023-09-12 16:37:16 -0300

    Avances en lógica

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,38 +1,46 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TextField from "@mui/material/TextField";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
 import axios from "axios";
-import { Box } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
-import EditIcon from '@mui/icons-material/Edit'
+import { Box } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import EditIcon from "@mui/icons-material/Edit";
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 100 },
-  { field: 'name', headerName: 'Nombre de Categoría', width: 200 },
-  {
-    field: 'edit',
-    headerName: 'Editar',
-    width: 100,
-    renderCell: (params) => (
-      <div style={{ cursor: 'pointer' }}>
-        <EditIcon color="primary" />
-      </div>
-    ),
-  },
-];
-
-
-const rows = [
-  { id: 1, name: 'Salado' },
-  { id: 2, name: 'Dulce' },
-  { id: 3, name: 'Bebidas' }
-];
 
 
 export const Categories = () => {
   const [name, setName] = useState("");
+  const [categories, setCategories] = useState([]);
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 100 },
+    { field: "name", headerName: "Nombre de Categoría", width: 200 },
+    {
+      field: "edit",
+      headerName: "Editar",
+      width: 100,
+      renderCell: (params) => (
+        <div style={{ cursor: "pointer" }}>
+          <EditIcon
+            color="primary"
+            onClick={() => handleEdit(params.row.id)}
+            style={{ display: params.row.selected ? "block" : "none" }}
+          />
+        </div>
+      ),
+    },
+  ];
+
+  useEffect(() => {
+    allCategories();
+  }, []);
+
+  const handleEdit = (id) => {
+    
+    console.log(`Editar la fila con ID ${id}`);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -42,24 +50,40 @@ export const Categories = () => {
       .then((res) => {
         console.log("Categoría creada:", res.data);
         setName("");
+        allCategories();
       })
       .catch((error) => {
         console.error("Error al crear categoría:", error);
       });
   };
 
+  const allCategories = () => {
+    axios
+      .get("http://localhost:3001/admin/categories")
+      .then((res) => {
+        setCategories(res.data);
+      })
+      .catch((error) => {
+        console.error("Error al traer las categorías", error);
+      });
+  };
+
+
 
   return (
     <>
-      <div style={{ position: "fixed", top: 100, right: 15, height: 400, width: "70%" }}>
+      <div
+        style={{
+          position: "fixed",
+          top: 100,
+          right: 15,
+          height: 400,
+          width: "70%",
+        }}
+      >
         <DataGrid
-          rows={rows}
+          rows={categories}
           columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: { page: 0, pageSize: 5 },
-            },
-          }}
           pageSizeOptions={[5, 10]}
           checkboxSelection
         />
