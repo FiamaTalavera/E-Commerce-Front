beb2a7ab5281dfc4122eb5efb3cde77c051beca7
Agustin Torroja <atorroja18@gmail.com>
2023-09-14 20:25:39 -0300

    verificacion de usuario admin para acceder a la ruta de administrados

--- src/App.js
+++ src/App.js
@@ -1,83 +1,116 @@
-import './App.css';
-import { Route, Routes } from 'react-router';
-import Navbar from './components/Navbar';
-import Grid from './components/Grid';
-import Login from './components/Login';
-import { useEffect, useState } from 'react';
-import Register from './components/Register';
-import axios from 'axios';
-import { Cart } from './components/Cart';
-import { Categories } from './components/Categories';
-import Sidebar from './components/Sidebar';
-import Checkout from './components/Checkout/Checkout';
-import { Home } from './components/Home';
-import { Searchbar } from './components/Searchbar';
-import History from './components/History';
+import "./App.css";
+import { Route, Routes } from "react-router";
+import Navbar from "./components/Navbar";
+import Grid from "./components/Grid";
+import Login from "./components/Login";
+import { useEffect, useState } from "react";
+import Register from "./components/Register";
+import axios from "axios";
+import { Cart } from "./components/Cart";
+import { Categories } from "./components/Categories";
+import Sidebar from "./components/Sidebar";
+import Checkout from "./components/Checkout/Checkout";
+import { Home } from "./components/Home";
+import { Searchbar } from "./components/Searchbar";
+import History from "./components/History";
+import { useNavigate } from "react-router";
 
 function App() {
-    const [user, setUser] = useState(null);
-    const [products, setProducts] = useState([]);
-    const [searchedProducts, setSearchProducts] = useState([])
+  const [user, setUser] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [searchedProducts, setSearchProducts] = useState([]);
 
-    useEffect(() => {
-        const storedAuth = localStorage.getItem('user');
-        if (storedAuth) {
-            setUser(JSON.parse(storedAuth));
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const storedAuth = localStorage.getItem("user");
+    if (storedAuth) {
+      setUser(JSON.parse(storedAuth));
+    }
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_URLBACK}/products`)
+      .then((res) => {
+        console.log("Los productos --> ", res.data); /* sacar console log? */
+        setProducts(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const updateUser = (userAuth) => {
+    setUser(userAuth);
+  };
+
+  const handleLogout = () => {
+    axios
+      .post(`${process.env.REACT_APP_URLBACK}/user/logout`, null, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        if (response.status === 204) {
+          localStorage.removeItem("user");
+          setUser(null);
+        } else {
+          console.error("Error al cerrar sesión");
         }
-    }, []);
+      })
+      .catch((error) => {
+        console.error("Error al cerrar sesión:", error);
+      });
+  };
 
-        useEffect(() => {
-            axios
-                .get(`${process.env.REACT_APP_URLBACK}/products`)
-                .then((res) => {
-                    console.log('Los productos --> ', res.data); /* sacar console log? */
-                    setProducts(res.data);
-                })
-                .catch((err) => console.log(err));
-        }, []);
+  const onSearch = (searchRes) => {
+    setSearchProducts(searchRes);
+  };
 
-    const updateUser = (userAuth) => {
-        setUser(userAuth);
-    };
+  return (
+    <div>
+      <Navbar
+        user={user}
+        handleLogout={handleLogout}
+        clearSearch={() => onSearch([])}
+      />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              onSearch={onSearch}
+              searchedProducts={searchedProducts}
+              products={products}
+            />
+          }
+        />
+        <Route path="/user/register" element={<Register />} />
+        <Route path="/user/login" element={<Login updateUser={updateUser} />} />
+        <Route path="/order" element={<Cart />} />
 
-    const handleLogout = () => {
-        axios
-            .post(`${process.env.REACT_APP_URLBACK}/user/logout`, null, {
-                withCredentials: true,
-            })
-            .then((response) => {
-                if (response.status === 204) {
-                    localStorage.removeItem('user');
-                    setUser(null);
-                } else {
-                    console.error('Error al cerrar sesión');
-                }
-            })
-            .catch((error) => {
-                console.error('Error al cerrar sesión:', error);
-            });
-    };
+        {user && user.is_admin ? (
+          <>
+            <Route path="/admin" element={<Sidebar />} />
+            <Route path="/admin" element={<Categories />} />
+          </>
+        ): <Route path="/admin" element={<div> 
+        Acceso Denegado
+        <button onClick={() => navigate("/")}> Volver al Inicio </button>
+        </div>} />}
 
-        const onSearch = (searchRes) => {
-            setSearchProducts(searchRes);
-        };
-
-    return (
-        <div>
-            <Navbar user={user} handleLogout={handleLogout} clearSearch={()=>onSearch([])}/>
-            <Routes>
-                <Route path="/" element={<Home onSearch={onSearch} searchedProducts={searchedProducts} products={products} />} />
-                <Route path="/user/register" element={<Register />} />
-                <Route path="/user/login" element={<Login updateUser={updateUser} />} />
-                <Route path="/order" element={<Cart />} />
-                <Route path="/admin" element={<Sidebar />} />
-                <Route path="/admin" element={<Categories />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route path="/search" element={<Searchbar onSearch={onSearch} searchedProducts={searchedProducts} />} />
-                <Route path="/user/history" element={<History />} />
-            </Routes>
-        </div>
-    );
+        <Route path="/checkout" element={<Checkout />} />
+        <Route
+          path="/search"
+          element={
+            <Searchbar
+              onSearch={onSearch}
+              searchedProducts={searchedProducts}
+            />
+          }
+        />
+        <Route path="/user/history" element={<History />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
