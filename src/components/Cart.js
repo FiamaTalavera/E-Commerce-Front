420d1af9ba10f8a3232032e72a1745ace5be255c
Isidro-Molina <isidromolina260@gmail.com>
2023-09-15 11:13:05 -0300

    boton para vaciar chango

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchCartItems, incrementQuantity, decrementQuantity, removeFromCart } from '../state/cart';
+import { fetchCartItems, incrementQuantity, decrementQuantity, removeFromCart, clearCart } from '../state/cart';
 import { toast } from 'react-toastify';
 
 export const Cart = () => {
@@ -55,13 +55,35 @@ export const Cart = () => {
         axios
             .delete(`${process.env.REACT_APP_URLBACK}/order/remove/${orderId}/${productId}`)
             .then((res) => {
-                toast.warn('Producto removido :(')
+                toast.warn('Producto removido :(');
                 dispatch(removeFromCart({ orderId, productId }));
             })
             .catch((error) => {
                 toast.error('Error al remover');
                 console.error('Error al remover producto', error);
             });
+    };
+
+    const handleClear = (item) => {
+        const userId = item.userId;
+        axios
+            .delete(`${process.env.REACT_APP_URLBACK}/order/clearCart/${userId}`)
+            .then((res) => {
+                toast.success('Chango eliminado correctamente!');
+                dispatch(clearCart());
+            })
+            .catch((error) => {
+                console.error('Error al borrar');
+            });
+    };
+
+    const handleClearCartWithItem = () => {
+        if (cartItems.length > 0) {
+            const itemToClear = cartItems[0];
+            handleClear(itemToClear);
+        } else {
+            toast.error('No tenes nada para borrar!');
+        }
     };
 
     return (
@@ -79,10 +101,12 @@ export const Cart = () => {
                     </li>
                 ))}
             </ul>
+
             <h4>TOTAL A PAGAR: {cartItems.length > 0 ? totalPrice : 0} </h4>
             <Link to="/checkout">
                 <button>Ir a pagar</button>
             </Link>
+            <button onClick={handleClearCartWithItem}>ELIMINAR TODO</button>
         </div>
     );
 };
